fdc8acea12f0fe026cf34aba2dbc06c60627b4b6
Dayna Weststrate <daynakade@shaw.ca>
2023-01-16 14:42:09 -0800

    made figures comparing historic vs predicted temperatures

--- Data/climate_historicvspredict.R
+++ Data/climate_historicvspredict.R
@@ -293,3 +293,264 @@ ggplot() +
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+
+#Compare historical precipitation vs. predicted precipitation for each park and SSP
+
+#Fintry
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "fintry"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "fintry" & climate$ssp == "1-2.6"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Fintry Precipitation (SSP 1-2.6)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "fintry"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "fintry" & climate$ssp == "2-4.5"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Fintry Precipitation (SSP 2-4.5)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "fintry"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "fintry" & climate$ssp == "3-7.0"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Fintry Precipitation (SSP 3-7.0)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "fintry"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "fintry" & climate$ssp == "5-8.5"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Fintry Precipitation (SSP 5-8.5)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+
+#Manning
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "manning"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "manning" & climate$ssp == "1-2.6"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Manning Precipitation (SSP 1-2.6)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "manning"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "manning" & climate$ssp == "2-4.5"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Manning Precipitation (SSP 2-4.5)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "manning"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "manning" & climate$ssp == "3-7.0"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Manning Precipitation (SSP 3-7.0)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "manning"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "manning" & climate$ssp == "5-8.5"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Manning Precipitation (SSP 5-8.5)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+
+
+#Skaha
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "skaha"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "skaha" & climate$ssp == "1-2.6"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Skaha Precipitation (SSP 1-2.6)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "skaha"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "skaha" & climate$ssp == "2-4.5"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Skaha Precipitation (SSP 2-4.5)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "skaha"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "skaha" & climate$ssp == "3-7.0"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Skaha Precipitation (SSP 3-7.0)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+ggplot() +
+  geom_boxplot(data = ok2019_2022[which(ok2019_2022$park == "skaha"),], aes(x = month, y = avgprecip, group = cut_width(month, 1))) +
+  geom_point(data = climate[which(climate$park == "skaha" & climate$ssp == "5-8.5"),], aes(x = month, y = avgprecip, col = period), size = 2) +
+  #scale_y_continuous(limits = c(-10,30)) +
+  scale_color_viridis(discrete = T) +
+  scale_x_continuous(breaks = seq_along(month.abb), labels = month.abb) +
+  xlab("Month") +
+  ylab("Avg Precipitation (mm)") +
+  ggtitle("Skaha Precipitation (SSP 5-8.5)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_text(size=12, family = "sans", face = "bold"),
+        axis.title.x = element_text(size=12, family = "sans", face = "bold"),
+        axis.text.y = element_text(size=10, family = "sans"),
+        axis.text.x  = element_text(size=8, family = "sans"),
+        plot.title = element_text(hjust = -0.05, size = 12, family = "sans", face = "bold"),
+        legend.position = "right",
+        panel.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = NA),
+        plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
