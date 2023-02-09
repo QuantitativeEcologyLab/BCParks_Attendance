97e85ab368f553f4912f5c21914ded7009600407
Stefano Mezzini <stefano@menouno.com>
2023-02-09 11:43:41 -0800

    corrected file name

--- Scripts/datadownload/download-climate-data.R
+++ Scripts/datadownload/download-climate-data.R
@@ -20,23 +20,13 @@ for(y in 2010:2022) {
 }
 
 #' download climate data projections
-for(y in 2021:2100) { # 2021-2040 to 2081-2100
+for(y in 2021:2100) {
   cat('Downloading ', y, '...\n', sep = '') # to track progress
   projClimateNA(
-    file = 'area-dem-2021-11-27-low-res.csv',
+    file = 'parks-dem.csv',
     tFrame = 'M', # monthly averages
     exe = 'ClimateNA_v7.31.exe', # exe location (in working directory)
     scen = '8GCM', # 8GCMs_ensemble General Circulation Model
     ssp = c('S1', 'S2', 'S3', 'S5'), # Shared Socioeconomic Pathway scenarios
     years = as.character(y)) # can only extract data for two decades as a time
 }
-
-# convert csv to a TIFF (necessary for spatial cropping)
-csv_file <- read.csv('area-dem-2021-11-27-low-res/8GCMs_ensemble_ssp126_2021-2040.csv')
-CSVtoTIFF(Longitude = csv_file$Longitude,
-          Latitude = csv_file$Latitude,
-          Value = csv_file$Tmin01,
-          filename = 'test.tif',
-          outdir = '../data/weather-data')
-
-terra::plot(terra::rast('../data/weather-data/test.tif'))
