2396f55b4cf04d69e764ac5f84bb5678ccb8cbe1
Dayna Weststrate <daynakade@shaw.ca>
2023-02-13 12:01:40 -0800

    fixed mislabelled regions for 3 parks

--- Scripts/figures/map-bcparks.R
+++ Scripts/figures/map-bcparks.R
@@ -6,8 +6,7 @@ library('canadianmaps') # to download a shapefile of BC
 library('ggplot2')      # for fancy plots
 theme_set(theme_map())
 
-#Source in BC Parks data
-source("~/Desktop/bio/440/BCParks_Attendance/Scripts/datatidy/bcparks.R")
+# Import BC Parks data
 parks <- read.csv('~/Desktop/bio/440/BCParks_Attendance/Data/bcparks/park_coordinates.csv')
 
 # import a shapefile of British Columbia
@@ -19,7 +18,6 @@ bc_shp <-
 ggplot() +
   geom_sf(data = bc_shp) +
   geom_point(aes(longitude, latitude, col = region), parks, size = 2) +
-  labs(x = NULL, y = NULL) +
   scale_color_manual(name="Region", 
                      values=alpha(c('north'="orange",
                               'ok'="forestgreen",
@@ -34,3 +32,4 @@ ggplot() +
   theme(legend.title = element_text(size = 12),
         legend.text = element_text(size = 10),
         legend.box.background = element_rect(color = "black"))
+
