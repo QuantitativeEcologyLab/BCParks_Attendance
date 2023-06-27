bcd443e951ce9ed24159675d907480e1692a8e1a
Dayna Weststrate <daynakade@shaw.ca>
2023-06-27 14:07:11 -0700

    reorganized files

---
+++ Scripts/datadownload/Climate/download-climate-data.R
@@ -0,0 +1,32 @@
+library('climatenaR')   # to download climate data and projections
+
+#' if necessary, install the `climatenaR` package with
+#' `remotes::install_github('burnett-m/climatenaR', build_vignettes = TRUE)`
+#' *NOTE:* the `climatenaR` package requires the ClimateNA software.
+#'         See https://register.climatena.ca/ to download it
+
+#' change the working directory as required by `climatenaR`
+setwd('~/GitHub/BCParks_Attendance/climate-na/')
+
+#' download historical climate data
+for(y in 2010:2022) {
+  cat('Downloading ', y, '...\n', sep = '') # to track progress
+  histClimateNA(
+    file = 'parks-dem.csv',
+    dateR = as.character(y),
+    tFrame = 'M', # monthly averages
+    exe = 'ClimateNA_v7.31.exe',
+    outdir = 'dayna-s-park-climate-data') # exe location (in working directory)
+}
+
+#' download climate data projections
+for(y in 2021:2100) {
+  cat('Downloading ', y, '...\n', sep = '') # to track progress
+  projClimateNA(
+    file = 'parks-dem.csv',
+    tFrame = 'M', # monthly averages
+    exe = 'ClimateNA_v7.31.exe', # exe location (in working directory)
+    scen = '8GCM', # 8GCMs_ensemble General Circulation Model
+    ssp = c('S1', 'S2', 'S3', 'S5'), # Shared Socioeconomic Pathway scenarios
+    years = as.character(y))
+}
---
+++ Scripts/datadownload/Climate/download-dem.R
@@ -0,0 +1,52 @@
+library('dplyr')        # for data wrangling
+library('canadianmaps') # to download a shapefile of BC
+library('elevatr')      # to download digital elevation models
+library('purrr')        # for functional programming (map_***(), etc.)
+library('sp')           # for spatial data
+library('sf')           # for spatial data
+library('terra')        # for raster data
+
+#' if necessary, install the `climatenaR` package with
+#' `remotes::install_github('burnett-m/climatenaR', build_vignettes = TRUE)`
+#' *NOTE:* the `climatenaR` package requires the ClimateNA software.
+#'         See https://register.climatena.ca/ to download it
+
+# import a shapefile of British Columbia
+bc_shp <-
+  st_as_sf(PROV) %>% # convert to spatial features (sf) object
+  filter(PRENAME == 'British Columbia') %>% # filter to BC only
+  st_geometry() # extract boundaries only
+
+# convert all telemetry datasets to spatial data points ----
+locations_df <- read.csv('Data/parks/park_coordinates.csv')
+locations <- SpatialPoints(select(locations_df, longitude, latitude))
+
+ctmm::projection(locations) <- '+proj=longlat'
+
+# check locations
+plot(bc_shp)
+plot(locations, add = TRUE, col = 'red', pch = 19, cex = 0.5)
+
+# import a Digital Elevation Model (DEM) for the region(s) of interest
+#' requires the `progress` package
+dem <- get_elev_raster(locations = locations,
+                       z = 3,
+                       clip = 'bbox',
+                       expand = 0.1)
+
+plot(dem)
+plot(locations, add = TRUE, col = 'red', pch = 19, cex = 0.5)
+
+# write the csv
+# (circumventing climatenaR's functions because we only need 3 locations)
+locations_df <- mutate(locations_df,
+                       el = terra::extract(dem, locations),
+                       ID1 = 1:n(),
+                       ID2 = ID1) %>%
+  relocate(ID1:ID2)
+
+write.csv(locations_df, file = 'Data/parks-dem.csv', row.names = FALSE)
+
+# check the csv
+read.csv('Data/parks-dem.csv') %>%
+  head()
