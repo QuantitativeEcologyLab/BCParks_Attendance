e0697cd5dce7bd2816498944d98a3f41bee3d0a9
Dayna Weststrate <daynakade@shaw.ca>
2023-07-04 13:34:51 -0700

    added updates to figure 2

--- Scripts/figures/Attendance/figure2.R
+++ Scripts/figures/Attendance/figure2.R
@@ -1,22 +1,67 @@
+# Note: this script relies on previously fitted data and model objects.
+# It can take several hours to run 
+
+library(mgcv)
+library(marginaleffects)
+library(tidyverse)
 library(ggplot2)
 library(gridExtra)
 library(khroma)
 
-# Import the historical data
-bcparks = readRDS("~/Desktop/bio/440/BCParks_Attendance/Data/bcparks/bcparks.rds")
+#Load in the data and model
+bcparks <- readRDS("~/Downloads/bcparks.rds")
+regions <- aggregate(region ~ park, data = bcparks, FUN = "unique")
+model <- readRDS("~/Downloads/model.rds") #note: this needed to be manually changed from rda to rds
+
+# Number of parks to loop over
+PARKS <- levels(bcparks$park)
+N <- length(PARKS)
+
+#-------------------------------------------------------
+# Generate the temperature panel (A)
+#-------------------------------------------------------
+
+#Loop over the number of parks
+for(i in 1:N){
+  preds <- try(predictions(model,
+                       datagrid(park = levels(bcparks$park)[i],
+                                avgtemp = seq(min(bcparks$avgtemp),
+                                              max(bcparks$avgtemp),
+                                              by = 0.1))))
+  
+  #Bind the results
+  if(i == 1){res <- preds}
+  if(i>1 && class(preds)[1] == "predictions"){res <- rbind(res, preds)}
+  
+  #Clear the intermediate variable
+  rm(preds)
+}
+
+#Save predictions
+save(res, file = "~/Dropbox/UBC/Students/Dayna_Weststrate/Manuscript/temp_preds.Rda")
+load("~/Dropbox/UBC/Students/Dayna_Weststrate/Manuscript/temp_preds.Rda")
+
+# Merge and aggregate by region
+res2 <- merge(res, regions, by.x = "park", by.y = "park", all.x = T)
+temp_preds <- aggregate(estimate ~ avgtemp + region, data = res2, FUN = "mean")
+
+# Average, population level effect
+temp_pop_level <- aggregate(estimate ~ avgtemp, data = res2, FUN = "mean")
+
 
 # Plot attendance in response to temperature
 temp <-
-  ggplot(data = bcparks[which(bcparks$attendancetype == "dayuse"),], 
-         aes(y = attendance, x = avgtemp)) +
-  geom_point(aes(col = region),
-             alpha = 0.01) + # points for each observation
-  geom_line(stat="smooth",method = "gam",
-            aes(col = region),
-            alpha = 0.9, size = 0.7, se = F) + # line for each region
-  geom_line(stat="smooth",method = "gam",
-            alpha = 1, col = "black", size = 1.2, se = T) + # line for overall trend
-  scale_y_continuous(limits = c(0,10)) +
+  ggplot() +
+  geom_point(data = bcparks[which(bcparks$attendancetype == "dayuse"),], 
+             aes(y = attendance, x = avgtemp, col = region),
+             alpha = 0.05, shape = 16) + # points for each observation
+  # geom_line(data = temp_pop_level,
+  #           aes(x = avgtemp, estimate),
+  #           alpha = 1, col = "black", size = 1.2) + # line for overall trend
+  geom_line(data = temp_preds,
+            aes(x = avgtemp, estimate, col = region),
+            alpha = 0.9, size = 0.7) + # line for each region
+  scale_y_continuous(limits = c(0,15), expand = c(0,0.1)) +
   scale_colour_muted(name="Region",
                      labels=c('Northern', 
                               'Kootenay-Okanagan', 
@@ -26,7 +71,6 @@ temp <-
   xlab("Average Monthly Temperature (ºC)") +
   ylab("Monthly Park Visitors (per 1000 people)") +
   ggtitle("A") +
-  # labs(subtitle = "Attendance and Temperature") +
   guides(col = guide_legend(override.aes = list(alpha=1))) +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
@@ -35,7 +79,7 @@ temp <-
         axis.title.x = element_text(size=12, family = "sans", face = "bold"),
         axis.text.y = element_text(size=10, family = "sans"),
         axis.text.x  = element_text(size=8, family = "sans"),
-        plot.title = element_text(hjust = 0.04, vjust = -5, size = 35, 
+        plot.title = element_text(hjust = 0.04, vjust = -5, size = 20, 
                                   family = "sans", face = "bold"),
         # plot.subtitle = element_text(size = 14, family = "sans", face = "bold"),
         legend.position=c(0.21,0.66),
@@ -45,18 +89,54 @@ temp <-
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
 
+
+
+#-------------------------------------------------------
+# Generate the precipitation panel (B)
+#-------------------------------------------------------
+
+#Loop over the number of parks
+for(i in 1:N){
+  
+  preds <- try(predictions(model,
+                           datagrid(park = levels(bcparks$park)[i],
+                                    avgprecip = seq(min(bcparks$avgprecip),
+                                                  max(bcparks$avgprecip),
+                                                  by = 0.1))))
+  
+  #Bind the results
+  if(i == 1){res <- preds}
+  if(i>1 && class(preds)[1] == "predictions"){res <- rbind(res, preds)}
+  
+  #Clear the intermediate variable
+  rm(preds)
+}
+
+#Save predictions
+save(res, file = "~/Dropbox/UBC/Students/Dayna_Weststrate/Manuscript/precip_preds.Rda")
+
+# Merge and aggregate by region
+res3 <- merge(res, regions, by.x = "park", by.y = "park", all.x = T)
+precip_preds <- aggregate(estimate ~ avgprecip + region, data = res3, FUN = "mean")
+
+# Average, population level effect
+precip_pop_level <- aggregate(estimate ~ avgprecip, data = res3, FUN = "mean")
+
+
+
 # Plot attendance in response to precipitation
 precip <-
-  ggplot(data = bcparks[which(bcparks$attendancetype == "dayuse"),], 
-         aes(y = attendance, x = avgprecip)) +
-  geom_point(aes(col = region),
-             alpha = 0.01) + # points for each observation
-  geom_line(stat="smooth",method = "gam",
-            aes(col = region),
-            alpha = 0.9, size = 0.7, se = F) + # line for each region
-  geom_line(stat="smooth",method = "gam",
-            alpha = 1, col = "black", size = 1.2, se = T) + # line for overall trend
-  scale_y_continuous(limits = c(0,8)) +
+  ggplot() +
+  geom_point(data = bcparks[which(bcparks$attendancetype == "dayuse"),], 
+             aes(y = attendance, x = avgprecip, col = region),
+             alpha = 0.05, shape = 16) + # points for each observation
+  geom_line(data = precip_preds,
+            aes(x = avgprecip, estimate, col = region),
+            alpha = 0.9, size = 0.7) + # line for each region
+  # geom_line(data = precip_pop_level,
+  #           aes(x = avgprecip, estimate),
+  #           alpha = 1, col = "black", size = 1.2) + # line for overall trend
+  scale_y_continuous(limits = c(0,15), expand = c(0,0.1)) +
   scale_x_continuous(limits = c(0,30)) +
   scale_colour_muted(name="Region",
                      labels=c('Northern', 
@@ -67,7 +147,6 @@ precip <-
   xlab("Average Monthly Precipitation (mm)") +
   ylab("Monthly Park Visitors (per 1000 people)") +
   ggtitle("B") +
-  # labs(subtitle = "Attendance and Precipitation") +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -75,14 +154,14 @@ precip <-
         axis.title.x = element_text(size=12, family = "sans", face = "bold"),
         axis.text.y = element_text(size=10, family = "sans"),
         axis.text.x  = element_text(size=8, family = "sans"),
-        plot.title = element_text(hjust = 0.04, vjust = -5, size = 35, 
+        plot.title = element_text(hjust = 0.04, vjust = -5, size = 20, 
                                   family = "sans", face = "bold"),
-        # plot.subtitle = element_text(size = 10, family = "sans", face = "bold"),
         legend.position = "none",
         legend.box.background = element_rect(color = "black"),
         panel.background = element_rect(fill = "transparent"),
         plot.background = element_rect(fill = "transparent", color = NA),
         plot.margin = unit(c(0.2,0.1,0.2,0.2), "cm"))
+
 
 # Combine the 2 panels
 FIG2 <- grid.arrange(temp, precip, 
@@ -93,5 +172,4 @@ ggsave(FIG2,
        width = 10, height = 5, units = "in",
        dpi = 600,
        bg = "white",
-       file="~/Desktop/bio/440/BCParks_Attendance/Figures/figure2.png")
-
+       file="~/Dropbox/UBC/Students/Dayna_Weststrate/Manuscript/figure2.png")
